35c98f3ba072796e17826f9dcfcefc1a17b1daf2
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-11 23:27:09 -0500

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
-#reason of this script is that dockerfile only execute one command at the time but we need sometimes at the moment we create 
-#the docker image to run more that one software for expecified configuration like when you need mysql running to chnage or create
-#database for the container ...
+cd /opt/
+wget http://opensimulator.org/dist/opensim-0.8.0.3.tar.gz
+tar xvfvz opensim-0.8.0.3.tar.gz
+rm opensim-0.8.0.3.tar.gz
