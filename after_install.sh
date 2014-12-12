948f365ba120cbecfe1d71a12c940c7d686733e9
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-12 02:12:36 -0500

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -2,6 +2,7 @@
 ### In after_install.sh (make sure this file is chmod +x):
 
 # run initial configuration script ...
+/opt/opensim-0.8.0.3/bin/opensim-ode.sh
 
-
-#rename   unrun to run script 
+#rename   unrun to run script
+mv /etc/service/opensim/unrun /etc/service/opensim/run
