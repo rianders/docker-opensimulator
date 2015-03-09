8120e91501429084750ee18030947a2e032660a4
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-03-09 14:29:38 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /opt/
-wget http://opensimulator.org/dist/opensim-0.8.0.3.tar.gz
-tar xvfvz opensim-0.8.0.3.tar.gz
-rm opensim-0.8.0.3.tar.gz
+wget http://opensimulator.org/dist/opensim-0.8.1-rc2.tar.gz
+tar xvfvz opensim-0.8.1-rc2.tar.gz
+rm opensim-0.8.1-rc2.tar.gz
