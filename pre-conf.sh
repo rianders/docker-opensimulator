994aac8348c979e845cc8a4cc1f307e497b1d88c
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-05-17 21:21:22 -0400

    Update pre-conf.sh
    I added mono  4.0.1 build to check if fix problem of the container taking to much resource from cpu.

--- pre-conf.sh
+++ pre-conf.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# installing mono-4.0.1
+wget http://download.mono-project.com/sources/mono/mono-4.0.1.tar.bz2 \
+  | tar -xjv --directory=/tmp
+cd /tmp/mono-4.0.1
+./configure --prefix=/usr/local
+make
+make install
+cd /tmp
+rm -rf /tmp/mono-4.0.1
+
+
+# installing opensim-0.8.1
 cd /opt/
 wget http://opensimulator.org/dist/opensim-0.8.1.tar.gz
 tar xvfvz opensim-0.8.1.tar.gz
