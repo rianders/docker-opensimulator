8a3f3a2390db163c2ba1532ce7bb5aed8a46169a
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-05-20 10:46:45 -0400

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -6,8 +6,5 @@ cd /opt/opensim-0.8.1/bin/
 cp pCampBot.ini.example pCampBot.ini
 ./opensim-ode.sh
 
-#rename   unrun to run script
-mv /etc/service/opensim/unrun /etc/service/opensim/run
-
 #remove itself
 rm /sbin/after_install
