9494832c89f59154fce16eebfe884e714c4508af
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-05-20 10:38:49 -0400

    Delete opensim.sh

--- opensim.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-### In opensim.sh (make sure this file is chmod +x):
-# `/sbin/setuser mysql` runs the given command as the user `mysql`.
-# If you omit that part, the command will be run as root.
-
-cd /opt/opensim-0.8.1/bin/
-exec mono ./OpenSim.exe >>/var/log/opensim.log 2>&1
