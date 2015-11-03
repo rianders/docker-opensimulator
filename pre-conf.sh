48c27695c79a9b8c24e135da234ca4365e5e4aa7
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-11-03 02:37:22 -0500

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# installing opensim-0.8.1
+# installing opensim-0.8.2.0-rc1
 cd /opt/
-wget http://opensimulator.org/dist/opensim-0.8.1.1.tar.gz
-tar xvfvz opensim-0.8.1.1.tar.gz
-mv /opt/opensim-0.8.1.1 /opt/opensim
-rm opensim-0.8.1.1.tar.gz
+wget http://opensimulator.org/dist/opensim-0.8.2.0-rc1.tar.gz
+tar xvfvz opensim-0.8.2.0-rc1.tar.gz
+mv /opt/opensim-0.8.2.0-rc1 /opt/opensim
+rm opensim-0.8.2.0-rc1.tar.gz
