69e611b4f24e81f1d9dd90b54d23090f18e0d5f9
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2020-01-12 22:08:20 -0500

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-opensim_version=0.9.0.1
-# installing opensim-0.9.0.1
+opensim_version=0.9.1.1
+# installing opensim-0.9.1.1
 cd /opt/
 wget http://opensimulator.org/dist/opensim-${opensim_version}.tar.gz
 tar xvfvz opensim-${opensim_version}.tar.gz
